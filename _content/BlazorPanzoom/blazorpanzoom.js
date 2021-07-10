482bde3e8f06a4bd2b4b4e3a96b050fc95db41c6
shaigem <2762027+shaigem@users.noreply.github.com>
2021-07-10 23:15:23 +0000

    Deploying to gh-pages from @ shaigem/BlazorPanzoom@10a92bdd5a93c2a6be222130265988ad6a028c71 🚀

---
+++ _content/BlazorPanzoom/blazorpanzoom.js
@@ -0,0 +1,39 @@
+﻿class BlazorPanzoomInterop {
+
+    constructor() {
+        this.boundWheelListener = null;
+    }
+
+    createPanzoomForReference(element, options) {
+        return Panzoom(element, options)
+    }
+
+    registerDefaultWheelZoom(element, panzoom) {
+        element.parentElement.addEventListener('wheel', panzoom.zoomWithWheel)
+    }
+
+    registerWheelListener(dotnetReference, element) {
+        element.parentElement.addEventListener('wheel', this.boundWheelListener = this.wheelHandler.bind(this, dotnetReference))
+    }
+
+    wheelHandler(dotnetReference, event) {
+        dotnetReference.invokeMethodAsync('OnCustomWheelEvent', {
+            deltaX: event.deltaX,
+            deltaY: event.deltaY,
+            clientX: event.clientX,
+            clientY: event.clientY,
+            shiftKey: event.shiftKey,
+            altKey: event.altKey
+        })
+    }
+
+    dispose(element, panzoom) {
+        element.parentElement.removeEventListener('wheel', panzoom.zoomWithWheel);
+        if (this.boundWheelListener) {
+            element.parentElement.removeEventListener('wheel', this.boundWheelListener);
+        }
+    }
+
+}
+
+window.blazorPanzoom = new BlazorPanzoomInterop()
