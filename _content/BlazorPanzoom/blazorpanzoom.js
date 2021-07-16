059d2e947c09c8c58505c485aa66b693a6a20a06
shaigem <2762027+shaigem@users.noreply.github.com>
2021-07-16 15:52:23 +0000

    Deploying to gh-pages from @ shaigem/BlazorPanzoom@6ffb5b2692bbed53cec12a1b70911e24503d13f0 🚀

--- _content/BlazorPanzoom/blazorpanzoom.js
+++ _content/BlazorPanzoom/blazorpanzoom.js
@@ -4,7 +4,9 @@
     }
 
     createPanzoomForReference(element, options) {
-        return Panzoom(element, options)
+        const panzoom = Panzoom(element, options)
+        panzoom.boundElement = element
+        return panzoom
     }
 
     createPanzoomForSelector(selector, options) {
@@ -13,7 +15,7 @@
             const array = []
             for (let i = 0; i < elements.length; i++) {
                 const element = elements[i]
-                array.push(DotNet.createJSObjectReference(Panzoom(element, options)))
+                array.push(DotNet.createJSObjectReference(this.createPanzoomForReference(element, options)))
             }
             return array
         } catch {
@@ -27,21 +29,24 @@
         }
 
         for (let i = 0; i < panzoomList.length; i++) {
-            const ref = panzoomList[i].jsPanzoomReference
+            const ref = panzoomList[i]
             ref[functionName](args)
         }
     }
 
-    registerDefaultWheelZoom(element, panzoom) {
-        element.parentElement.addEventListener('wheel', panzoom.zoomWithWheel)
+    registerZoomWithWheel(panzoom, element) {
+        const parent = element ? element.parentElement : panzoom.boundElement.parentElement
+        parent.addEventListener('wheel', panzoom.zoomWithWheel)
     }
 
-    registerWheelListener(dotnetReference, element, panzoom) {
+    registerWheelListener(dotnetReference, panzoom, element) {
+        const parent = element ? element.parentElement : panzoom.boundElement.parentElement
         panzoom.dotNetWheelListenerReference = dotnetReference
-        element.parentElement.addEventListener('wheel', panzoom.boundWheelListener = this.wheelHandler.bind(this, dotnetReference))
+        parent.addEventListener('wheel', panzoom.boundWheelListener = this.wheelHandler.bind(this, dotnetReference))
     }
 
     wheelHandler(dotnetReference, event) {
+        event.preventDefault()
         dotnetReference.invokeMethodAsync('OnCustomWheelEvent', {
             deltaX: event.deltaX,
             deltaY: event.deltaY,
@@ -52,12 +57,24 @@
         })
     }
 
-    removeWheelListeners(element, panzoom) {
-        element.parentElement.removeEventListener('wheel', panzoom.zoomWithWheel);
+    removeZoomWithWheel(panzoom, element) {
+        const parent = element ? element.parentElement : panzoom.boundElement.parentElement
+        parent.removeEventListener('wheel', panzoom.zoomWithWheel);
+    }
+
+    removeWheelListener(panzoom, element) {
+        const parent = element ? element.parentElement : panzoom.boundElement.parentElement
         if (panzoom.boundWheelListener) {
-            element.parentElement.removeEventListener('wheel', panzoom.boundWheelListener);
+            parent.removeEventListener('wheel', panzoom.boundWheelListener);
             panzoom.dotNetWheelListenerReference.dispose()
             delete panzoom.dotNetWheelListenerReference
+            delete panzoom.boundWheelListener
+        }
+    }
+
+    destroyPanzoom(panzoom) {
+        if (panzoom) {
+            delete panzoom.boundElement
         }
     }
 }
